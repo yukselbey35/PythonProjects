f860d35fbfa8c3b6f6b6dacdba5ce4a130ced239
yukselbey35 <yukselbey35@gmail.com>
2020-06-17 10:58:52 -0700

    testing

---
+++ python00.py
@@ -0,0 +1,6 @@
+print("merhaba python")
+
+print(13+19)
+
+
+
---
+++ python01.py
@@ -0,0 +1,29 @@
+# Açıklama satırı
+print("Çift tırnak string")
+
+print('tek tırnakla yazım')
+
+print("""üç tırnakla""")
+
+print(""" Araya "çift tırnakla" yazı yazalım """)
+
+print(""" <a href="www.yasinhoca.com">  """)
+
+print("birinci satır \n ikinci satır")
+
+print("\t tab işi \t\t bir tab daha")
+
+#numbers
+#string
+
+a = 3 #integer algılar
+b = 3.0 #float olduğunu algılar
+c = "string değişken" #string
+
+d = int(15) #değişken tipini belirtiyorsunuz
+e = float(3.45)
+f = str("metin değişken")
+
+toplam = a + b
+
+print("%s + %s = %s"%(a,b,toplam))
